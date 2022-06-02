36480514aa04c29ec0bb0785c40fc4be5c43fdfa
abhilash29j <102944572+abhilash29j@users.noreply.github.com>
2022-06-02 16:41:18 +0530

    Initial commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+public class Student implements  Runnable{
+    private int id;
+    private Book[] books;
+    private Random random;
+
+
+
+    public Student(int id , Book[] books) {
+        this.id = id;
+        this.books = books;
+        this.random = new Random();
+
+    }
+
+
+    @Override
+
+    public void run() {
+        while (true) {
+
+            int bookId = random.nextInt(Constants.NUM_OF_BOOKS);
+            try {
+                books[bookId].read(this);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+         }
+    }
+
+    public String toString() {
+        return "Student # " + id + "]";
+    }
+
+
+
+}
